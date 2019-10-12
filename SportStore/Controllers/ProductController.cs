2b65294942a3fa521390a3054e41fc489a0cfdba
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-11 21:43:00 -0400

    Add project files.

---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IPproductRepository repository;
+
+        public ProductController(IPproductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+
+
+
+    }
+}
