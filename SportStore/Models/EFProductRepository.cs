a0233034a3517ac60003bdc026087946f0b8a85c
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-10-11 22:03:51 -0400

    Done for today (before ch5)

---
+++ SportStore/Models/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class EFProductRepository : IPproductRepository
+    {
+        private ApplicationDbContext context;
+
+       public EFProductRepository (ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
